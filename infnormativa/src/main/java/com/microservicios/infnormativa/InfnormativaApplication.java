245c27825a0e9c598105720717e0ef70f43248f0
Guevara Villegas <rguevarav@migraciones.gob.pe>
2021-01-20 18:14:00 -0500

    20/01/2021 ver 1.0

---
+++ infnormativa/src/main/java/com/microservicios/infnormativa/InfnormativaApplication.java
@@ -0,0 +1,13 @@
+package com.microservicios.infnormativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfnormativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfnormativaApplication.class, args);
+	}
+
+}
